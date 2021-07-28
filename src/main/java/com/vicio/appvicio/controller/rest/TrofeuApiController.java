d29d59c1db568169203a0b6dcb373b608bc2f26a
Giovana C. R. da Silva <giovanacarolini@outlook.com>
2021-07-27 21:25:43 -0300

    controllers do rest

---
+++ src/main/java/com/vicio/appvicio/controller/rest/TrofeuApiController.java
@@ -0,0 +1,50 @@
+package com.vicio.appvicio.controller.rest;
+
+
+import com.vicio.appvicio.model.Trofeu;
+import com.vicio.appvicio.service.TrofeuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@Api(tags = "Trofeu")
+@RequestMapping("/api/trofeu")
+public class TrofeuApiController {
+    @Autowired
+    TrofeuService trofeuService;
+
+    @ApiOperation(value = "Listar")
+    @GetMapping
+    public List<Trofeu> listarTodosTrofeus(){
+        return trofeuService.listarTodos();
+    }
+
+    @ApiOperation(value = "Salvar")
+    @PostMapping
+    public ResponseEntity<Trofeu> salvarTrofeu (@Valid @RequestBody Trofeu Trofeu){
+        Trofeu TrofeuSalvar = trofeuService.salvarTrofeu(Trofeu);
+        return ResponseEntity.status(HttpStatus.CREATED).body(TrofeuSalvar);
+    }
+
+    @ApiOperation(value = "Atualizar")
+    @PutMapping("/{id}")
+    public ResponseEntity<Trofeu> atualizarTrofeu (@Valid Integer id, @PathVariable Trofeu Trofeu){
+        return ResponseEntity.ok(trofeuService.atualizarTrofeu(id, Trofeu));
+    }
+
+    @ApiOperation(value = "Deletar")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable Integer id){
+        trofeuService.deletar(id);
+    }
+
+
+}
